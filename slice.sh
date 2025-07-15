4179a2402a5679e59970ace2505250910682dbad
billa05 <workinguse5@gmail.com>
2025-07-16 00:29:57 +0530

    no need for a container to be running ig

--- slice.sh
+++ slice.sh
@@ -11,7 +11,7 @@ GCODE="gcode/${BASENAME}.gcode"
 STATS="stats/${BASENAME}_stats.txt"
 FINALJSON="output/${BASENAME}.json"
 
-docker run --name prusaslicer-test \
+docker run --rm \
   -v "$(pwd)/models:/models" \
   -v "$(pwd)/gcode:/gcode" \
   -v "$(pwd)/config:/config" \
